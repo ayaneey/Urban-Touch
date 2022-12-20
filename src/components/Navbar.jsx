b4b3a85396b4e1988b8031bd399a8f1ab2cccf51
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2022-12-20 14:55:10 +0000

    fixed some errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,70 @@
+import { Search } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
 	height: 60px;
-	background-color: black;
+`;
+
+const Wrapper = styled.div`
+	padding: 10px 20px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const Language = styled.span`
+	font-size: 14px;
+	cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+	border: 0.5px solid lightgray;
+	display: flex;
+	align-items: center;
+	margin-left: 25px;
+	padding: 5px;
+`;
+
+const Input = styled.input`
+	border: none;
+`;
+
+const Left = styled.div`
+	flex: 1;
+	display: flex;
+	align-items: center;
+`;
+const Center = styled.div`
+	flex: 1;
+`;
+
+const Logo = styled.h1`
+	font-weight: bold;
+`;
+
+const Right = styled.div`
+	flex: 1;
 `;
 
 const Navbar = () => {
-	return <Container>navbar</Container>;
+	return (
+		<Container>
+			<Wrapper>
+				<Left>
+					<Language>EN</Language>
+					<SearchContainer>
+						<Input />
+						<Search />
+					</SearchContainer>
+				</Left>
+				<Center>
+					<Logo>Urban Touch</Logo>
+				</Center>
+				<Right>right</Right>
+			</Wrapper>
+		</Container>
+	);
 };
 
 export default Navbar;
