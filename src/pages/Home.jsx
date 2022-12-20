b18ba051547aef37838cc015d662b692d4f7fd89
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2022-12-20 14:11:46 +0000

    started styling navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 function Home() {
-	return <div>Homepage</div>;
+	return (
+		<div>
+			<Navbar />
+		</div>
+	);
 }
 
 export default Home;
